9f0e2250924011293a8632f0c9ef13c5748bcf78
Paiva <danpaiva@live.com>
2022-10-01 16:26:06 -0300

    feat: Alguns ajustes no program.cs

--- UsersMvcNet6/Program.cs
+++ UsersMvcNet6/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using UsersMvcNet6.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ScheduleContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionDev")));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -8,9 +14,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
