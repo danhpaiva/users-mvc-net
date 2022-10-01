fb72cb87c4783ddc09e677f19f34ab9e266db53a
Paiva <danpaiva@live.com>
2022-10-01 17:13:05 -0300

    feat: Ajuste na ContactController

--- UsersMvcNet6/Models/ErrorViewModel.cs
+++ UsersMvcNet6/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace UsersMvcNet6.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+  public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
